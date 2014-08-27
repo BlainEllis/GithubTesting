c157ec2d91957755ad74f581140f189f16989752
BlainEllis <TXD@Blains-iMac.local>
2014-08-27 12:21:51 +0100

    Initial Commit

---
+++ GithubTesting/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GithubTesting
+//
+//  Created by TXD on 27/08/2014.
+//  Copyright (c) 2014 TXD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GithubTesting/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GithubTesting
+//
+//  Created by TXD on 27/08/2014.
+//  Copyright (c) 2014 TXD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
